74865a0b6bf2318894aefdfa98a5f3db5865befc
Andrew Heiss <andrew@andrewheiss.com>
2025-09-22 13:50:25 -0400

    Initial commit

---
+++ build_data.R
@@ -0,0 +1,8 @@
+cheese <- tidytuesdayR::tt_load('2024-06-04')
+readr::write_csv(cheese$cheeses, "data/cheeses.csv")
+
+# Build answer key so that the plots to recreate exist in images/
+quarto::quarto_render(
+  "answers.qmd",
+  output_format = c("html", "typst")
+)
